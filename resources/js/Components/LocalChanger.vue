e32c2e5ef327d5481d1ab1f2ce6763a08dc20a8a
Alhada06 <Alhada06@gmail.com>
2021-10-26 00:22:11 +0100

    master

--- resources/js/Components/LocalChanger.vue
+++ resources/js/Components/LocalChanger.vue
@@ -1,6 +1,6 @@
 <template>
   <div >
-      <div class="flex flex-row  text-white mt-2" >
+      <div class="flex flex-row  text-white mt-2 " >
         <div  v-for="locale in $i18n.availableLocales" :key="`${locale}`">
           <LangLink @click="$i18n.locale=locale" :active="$i18n.locale===locale">
             {{locale}}
